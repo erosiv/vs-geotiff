7bc110a97e438ed7e852c9f130365e3af67779a4
weigert <nicholasmcdonald40@gmail.com>
2025-03-04 13:55:07 +0100

    moved some files around and imported a local package version for float64 support

---
+++ src/GeoTIFF.ts
@@ -0,0 +1,84 @@
+import * as tiff from 'tiff'
+import {Bitmap, shade} from './Bitmap'
+import { DataArray } from "tiff/lib/types";
+import * as cmap from "./cmap"
+
+enum BitmapShading {
+  None = "None",
+  Grayscale = "Grayscale",
+  Turbo = "Turbo",
+  Viridis = "Viridis"
+}
+
+export class GeoTIFF {
+  
+  private readonly _raw: DataArray;
+  private readonly _bytes: number;
+  private readonly _min: number;
+  private readonly _max: number;
+  _bitmap: Bitmap;
+  public _shade: BitmapShading;
+
+  constructor(source: Uint8Array | undefined){
+
+    if(source instanceof Uint8Array){
+
+      const ifd = tiff.decode(source)[0]
+      const kbytes = source.length/1000;
+      const width = ifd.width;
+      const height = ifd.height;
+
+      this._raw = ifd.data;
+      this._bytes = kbytes;
+      this._bitmap = new Bitmap(width, height)
+      
+      this._min = Number.MAX_VALUE
+      this._max = Number.MIN_VALUE
+      for(let p = 0; p < width*height; ++p){
+        const val = ifd.data[p];
+        this._min = Math.min(this._min, val)
+        this._max = Math.max(this._max, val)
+      }
+
+      this._shade = BitmapShading.Grayscale;
+      shade(this._bitmap, cmap.grayscale, this._raw, this._min, this._max)
+
+    } else {
+
+      this._raw = new Float32Array()
+      this._bytes = 0
+      this._bitmap = new Bitmap(0, 0)
+      this._min = 0.0
+      this._max = 0.0
+      this._shade = BitmapShading.None
+
+    }
+
+  }
+
+  public get bytes() { return this._bytes; }
+  public get width() { return this._bitmap._width; }
+  public get height() { return this._bitmap._height; }
+
+  public get min() { return this._min }
+  public get max() { return this._max }
+
+  //
+  // Shading Functions
+  //
+
+  public reshade(): Bitmap {
+    if(this._shade == BitmapShading.Grayscale){
+      this._shade = BitmapShading.Turbo;
+      shade(this._bitmap, cmap.turbo, this._raw, this._min, this._max)	
+    } else if(this._shade == BitmapShading.Turbo) {
+      this._shade = BitmapShading.Viridis;
+      shade(this._bitmap, cmap.viridis, this._raw, this._min, this._max)	
+    } else {
+      this._shade = BitmapShading.Grayscale;
+      shade(this._bitmap, cmap.grayscale, this._raw, this._min, this._max)
+    }
+    return this._bitmap
+  }
+
+}
--- src/GeoTIFFReadOnlyEditor.ts
+++ src/GeoTIFFReadOnlyEditor.ts
@@ -1,92 +1,11 @@
 import * as vscode from 'vscode';
 import { Disposable, disposeAll } from './dispose';
 import { getNonce } from './util';
-import * as tiff from 'tiff'
-import {Bitmap, shade} from './shade'
-import * as cmap from "./cmap"
+import { GeoTIFF } from './GeoTIFF'
 
-type DataArray = Uint8Array | Uint16Array | Float32Array;
-
-enum BitmapShading {
-	None = "None",
-  Grayscale = "Grayscale",
-  Turbo = "Turbo",
-	Viridis = "Viridis"
-}
-
-class GeoTIFFRaw {
-	
-	private readonly _raw: DataArray;
-	private readonly _bytes: number;
-	private readonly _min: number;
-	private readonly _max: number;
-	_bitmap: Bitmap;
-	public _shade: BitmapShading;
-
-	constructor(source: Uint8Array | undefined){
-
-		if(source instanceof Uint8Array){
-
-			const ifd = tiff.decode(source)[0]
-			const kbytes = source.length/1000;
-			const width = ifd.width;
-			const height = ifd.height;
-
-			this._raw = ifd.data;
-			this._bytes = kbytes;
-			this._bitmap = new Bitmap(width, height)
-			
-			this._min = Number.MAX_VALUE
-			this._max = Number.MIN_VALUE
-			for(let p = 0; p < width*height; ++p){
-				const val = ifd.data[p];
-				this._min = Math.min(this._min, val)
-				this._max = Math.max(this._max, val)
-			}
-
-			this._shade = BitmapShading.Grayscale;
-			shade(this._bitmap, cmap.grayscale, this._raw, this._min, this._max)
-
-		} else {
-
-			this._raw = new Float32Array()
-			this._bytes = 0
-			this._bitmap = new Bitmap(0, 0)
-			this._min = 0.0
-			this._max = 0.0
-			this._shade = BitmapShading.None
-
-		}
-
-	}
-
-	public get bytes() { return this._bytes; }
-	public get width() { return this._bitmap._width; }
-	public get height() { return this._bitmap._height; }
-
-	public get min() { return this._min }
-	public get max() { return this._max }
-
-	//
-	// Shading Functions
-	//
-
-	public reshade(): Bitmap {
-		if(this._shade == BitmapShading.Grayscale){
-			this._shade = BitmapShading.Turbo;
-			shade(this._bitmap, cmap.turbo, this._raw, this._min, this._max)	
-		} else if(this._shade == BitmapShading.Turbo) {
-			this._shade = BitmapShading.Viridis;
-			shade(this._bitmap, cmap.viridis, this._raw, this._min, this._max)	
-		} else {
-			this._shade = BitmapShading.Grayscale;
-			shade(this._bitmap, cmap.grayscale, this._raw, this._min, this._max)
-		}
-		return this._bitmap
-	}
-
-}
-
+//! GeoTIFFStatusBarInfo
+//! Static Status Bar Information Manager
+//!
 class GeoTIFFStatusBarInfo {
 
 	static itemColor: vscode.StatusBarItem;
@@ -107,7 +26,7 @@ class GeoTIFFStatusBarInfo {
 		this.itemBytes.hide();
 	}
 
-	public static updateStatusBar(geotiff: GeoTIFFRaw): void {
+	public static updateStatusBar(geotiff: GeoTIFF): void {
 		
 		GeoTIFFStatusBarInfo.itemColor.text = `$(symbol-color) ${geotiff._shade}`;
 		GeoTIFFStatusBarInfo.itemColor.show();
@@ -132,18 +51,18 @@ interface GeoTIFFDocumentDelegate {
 	getFileData(): Promise<Uint8Array>;
 }
 
-/**
- * Define the document (the data model) used for paw draw files.
- */
+//! GeoTIFFDocument
+//! Document State Wrapper
+//!
 class GeoTIFFDocument extends Disposable implements vscode.CustomDocument {
 
 	private readonly _uri: vscode.Uri;
 	private readonly _delegate: GeoTIFFDocumentDelegate;
-	readonly _raw: GeoTIFFRaw;
+	readonly _raw: GeoTIFF;
 
 	private constructor(
 		uri: vscode.Uri,
-		raw: GeoTIFFRaw,
+		raw: GeoTIFF,
 		delegate: GeoTIFFDocumentDelegate
 	) {
 
@@ -169,11 +88,11 @@ class GeoTIFFDocument extends Disposable implements vscode.CustomDocument {
 		return new GeoTIFFDocument(uri, fileData, delegate);
 	}
 
-	private static async readFile(uri: vscode.Uri): Promise<GeoTIFFRaw> {
+	private static async readFile(uri: vscode.Uri): Promise<GeoTIFF> {
 		if (uri.scheme === 'untitled') {
-			return new GeoTIFFRaw(undefined);
+			return new GeoTIFF(undefined);
 		}
-		return new GeoTIFFRaw(await vscode.workspace.fs.readFile(uri))
+		return new GeoTIFF(await vscode.workspace.fs.readFile(uri))
 	}
 
 	// Getters
@@ -185,18 +104,19 @@ class GeoTIFFDocument extends Disposable implements vscode.CustomDocument {
 	// Event Handlers
 
 	private readonly _onDidDispose = this._register(new vscode.EventEmitter<void>());
-	public readonly onDidDispose = this._onDidDispose.event;
 
 	public readonly _onDidChangeDocument = this._register(new vscode.EventEmitter<{
 		readonly content?: Uint8Array;
 	}>());
-	public readonly onDidChangeContent = this._onDidChangeDocument.event;
-
+	
 	public readonly _onDidChange = this._register(new vscode.EventEmitter<{
 		readonly label: string,
 		undo(): void,
 		redo(): void,
 	}>());
+	
+	public readonly onDidDispose = this._onDidDispose.event;
+	public readonly onDidChangeContent = this._onDidChangeDocument.event;
 	public readonly onDidChange = this._onDidChange.event;
 
 	dispose(): void {
